e21926856a62c8930499af5bf3cfc256386577ce
wxh <1297627104@qq.com>
2020-06-30 23:00:44 +0800

    添加vant ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="app"> 我是根组件</div>
+  <div class="app">
+    我是根组件
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-field v-model="value" label="文本" placeholder="请输入用户名" />
+    <br>
+    <div class="box"></div>
+  </div>
 </template>
-
 <script>
 export default {
-
+  data () {
+    return {
+      value: ''
+    }
+  }
 }
 </script>
-
 <style>
-.app{
-  color: red;
-  font-size: 40px;
+.box {
+ width: 300px;
+ height: 300px;
+ font-size: 75px;
+ background-color: #f60;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import Vant from 'vant'
+import { Button, Field } from 'vant'
+import 'vant/lib/index.css'
+// rem
+import 'amfe-flexible'
 // 关闭控制台生产提示
 Vue.config.productionTip = false
-
+// Vue.use(Vant)
+// 按需加载只需要使用需要的
+Vue.use(Button)
+Vue.use(Field)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
